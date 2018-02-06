96b88fa5b93a74f7b2d5825671b54c01432d4bc4
Pushpak Sharma <sharmatryhard@gmail.com>
2018-02-06 13:34:28 +0530

    Update Playfair_cipher.c

--- Practical Set - 1/Playfair_cipher.c
+++ Practical Set - 1/Playfair_cipher.c
@@ -159,4 +159,85 @@ int main()
 }
 
 */
+/*----6-02
 
+
+
+#include <stdio.h>
+#define m 5
+#define n 5
+//int count[1001] = {0};
+char mat [m][n];
+void remove_input_to_matrix(char str[],int size)
+{
+    int store[10001] = {0};
+    for(int i = 0;i<size;i++)
+    {
+        store[str[i]] = 1;
+    }
+    char input_matrix[25];
+    int flag = 0;
+    for(int i = 97;i<=122 ;i++)
+    {
+        if(!store[i])
+        {
+            input_matrix[flag++] = i;
+        }
+    }
+    int size_one = sizeof(input_matrix)/sizeof(input_matrix[0]);
+    for(int i = 0;i<size_one;i++)
+    {
+        printf("%c ",input_matrix[i]);
+    }
+   //return input_matrix;
+}
+int check_key(char str[],int size)
+{
+    char arr_return[101];
+    int flag = 0,count[250] = {0};
+    for(int i = 0; i<size ; i++)
+    {
+        if(count[str[i]] == 0)
+        {
+            count[str[i]] = 1;
+            arr_return[flag++] = str[i];
+        }
+    }
+    int flag1 = 4;
+    for(int i = 0;i < 5 ;i++)
+    {
+        for(int j = 0;j < 5; j++)
+        {
+            if(flag >= flag1)
+                mat[i][j] = arr_return[flag1++];
+            else
+                break;
+        }
+        
+    }
+    printf("%d",flag1);
+    return flag1;
+}
+int main()
+{
+    char str[] = {'h','e','l','l','o'};
+    int size = sizeof(str)/sizeof(str[0]);
+    int flag1;
+    //printf("\n%d\n",size);
+    //char input_matrix[25];
+    check_key(str,size);
+    //remove_input_to_matrix(str,size);
+    for(int i = 0;i < 5 ;i++)
+    {
+        for(int j = 0;j < 5; j++)
+        {
+            printf("%c ",mat[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+*/
